9fec601a153be1cc10f170817f9c9ddf410ce474
MasaharuDobashi <masaharu.dobashi@gmail.com>
2022-10-13 23:20:42 +0900

    first commit

---
+++ app/src/main/java/com/dobashi/todolist_jetpack/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.dobashi.todolist_jetpack
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.dobashi.todolist_jetpack.ui.theme.ToDoList_JetpackTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ToDoList_JetpackTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ToDoList_JetpackTheme {
+        Greeting("Android")
+    }
+}
