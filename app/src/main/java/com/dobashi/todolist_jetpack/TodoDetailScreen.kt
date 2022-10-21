fb14f9311014c831762398e9f3566e2654d11ef3
MasaharuDobashi <masaharu.dobashi@gmail.com>
2022-10-21 23:28:17 +0900

    詳細画面の編集

--- app/src/main/java/com/dobashi/todolist_jetpack/TodoDetailScreen.kt
+++ app/src/main/java/com/dobashi/todolist_jetpack/TodoDetailScreen.kt
@@ -3,11 +3,16 @@ package com.dobashi.todolist_jetpack
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.outlined.ArrowBack
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import kotlinx.coroutines.runBlocking
 
@@ -21,6 +26,11 @@ fun TodoDetailScreen(
     val model = runBlocking {
         TodoApplication.database.todoDao().getTodo(createTime)
     }
+
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -35,18 +45,68 @@ fun TodoDetailScreen(
                             contentDescription = "Back",
                         )
                     }
-                }
+                },
+                actions = {
+                    IconButton(onClick = { expanded = !expanded }) {
+                        Icon(Icons.Default.MoreVert, "")
+                    }
+                    DropdownMenu(
+                        expanded = expanded,
+                        onDismissRequest = { expanded = false },
+                    ) {
+                        DropdownMenuItem(onClick = { /*TODO*/ }) {
+                            Text(text = stringResource(id = R.string.edit))
+                        }
 
+                        DropdownMenuItem(onClick = { /*TODO*/ }) {
+                            Text(text = stringResource(id = R.string.delete))
+                        }
+                    }
+                }
             )
         },
     ) {
         Column(
             modifier
+                .padding(top = 8.dp)
                 .fillMaxSize()
         ) {
-            Text(text = model.todoDate)
-            Text(text = model.todoTime)
-            Text(text = model.toDoDetail)
+            DetailCard(
+                title = stringResource(id = R.string.date_title),
+                value = "${model.todoDate}\n${model.todoTime}"
+            )
+
+            DetailCard(
+                title = stringResource(id = R.string.detail_title),
+                value = model.toDoDetail
+            )
         }
     }
 }
+
+
+@Composable
+private fun DetailCard(title: String, value: String) {
+    Card(
+        elevation = 8.dp,
+        modifier = Modifier
+            .padding(
+                horizontal = 17.dp,
+                vertical = 8.dp
+            )
+            .fillMaxWidth()
+    ) {
+        Column(
+            modifier = Modifier.padding(
+                horizontal = 18.dp,
+                vertical = 8.dp
+            )
+        ) {
+            Text(text = title)
+            Text(text = value)
+        }
+    }
+
+}
+
+
